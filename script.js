6a51d05a62e95222aa46942bda1ffa15ccc28035
v-ki07 <viconu2007@gmail.com>
2024-06-09 16:32:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let w= 56;
+let y= 56.7;
+let v= false;
+let z= null;
+let c= undefined;
+let h= "Hello World"
+
+console.log()
+let user ="peter"
+alert(`welcome to csc 104 $(User)`)
+
+let ask =prompt ("what day of the week is toda?")
+alert(ask)
+
